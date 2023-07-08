555c1cb3ea51bedd489e866172ecf08812e078c3
Maryhei <makemak123@gmail.com>
2023-07-08 11:59:54 +0100

    Modified drag adn drop introduction to your image instead of your file

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
             </p>
           </div> : 
           <p>
-            Drag and frop your file here or
+            Drag and frop your image here or
           </p> }
           <button onClick={handleUpload} className='uploadButton'>Upload</button>
           <p>
